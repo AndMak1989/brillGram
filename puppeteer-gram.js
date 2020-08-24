8f59e0f4da5643bce18e3fd424ebf6bc1fd00fda
makand <makand1989@gmail.com>
2020-08-24 12:56:45 +0300

    test

--- puppeteer-gram.js
+++ puppeteer-gram.js
@@ -42,8 +42,11 @@ const accounts = [
             const sharedData = await page.evaluate(() => {
                 return window._sharedData
             });
+
             console.log(sharedData);
             const mediasObject = sharedData['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges'];
+
+            console.log(mediasObject);
 
             const mediaAll = [];
 
